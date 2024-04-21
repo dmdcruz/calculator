7129cf5cca40e032bdac0d685ff7e45ff3ded2a4
David Cruz <dmd.cruz12@gmail.com>
2024-04-21 19:47:06 +0100

    Write HTML page and developed calculator logic on the javascript

---
+++ script.js
@@ -0,0 +1,87 @@
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+const display = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".btnNumber");
+const operatorButtons = document.querySelectorAll(".btnOperator");
+const equalButton = document.querySelector(".btnEqual");
+const clearButton = document.querySelector(".btnClear");
+
+//Save operator in variable
+operatorButtons.forEach(((operatorButton) => {
+    operatorButton.addEventListener("click", () => {
+        operator = operatorButton.textContent;
+    });
+}));
+
+
+
+//Populate display fater click in one number
+numberButtons.forEach(((numberButton) => {
+    numberButton.addEventListener("click",() => {
+        display.textContent = numberButton.textContent;
+        if (operator === "") {
+            firstNumber = parseFloat(numberButton.textContent);
+        }
+        if (operator !== "") {
+            secondNumber = parseFloat(numberButton.textContent);
+        }
+    });
+}));
+
+//trigger operate function when = is clicked
+equalButton.addEventListener("click", () => {
+    let result = operate(operator, firstNumber, secondNumber);
+    display.textContent = result;
+    firstNumber = result;
+    operator = "";
+});
+
+//trigger clear function when clear btn is clicked;
+clearButton.addEventListener("click" ,() => {
+    clear();
+    display.textContent = "";
+});
+
+//operate function
+function operate(operator, firstNumber, secondNumber) {
+    let result = 0;
+    
+    if (operator == "+") {
+        result = Add(firstNumber, secondNumber);
+    }
+    if (operator == "-") {
+        result = Subtract(firstNumber, secondNumber);       
+    }
+    if (operator == "x") {
+        result = Multiply(firstNumber, secondNumber);    
+    }
+    if (operator == "/") {
+        result = Divide(firstNumber, secondNumber);
+    }
+    return result;
+};
+//function to perform addition
+function Add(firstNumber,secondNumber) {
+    return firstNumber + secondNumber;
+};
+//function to calculate subtraction
+function Subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+};
+//function to calculate multiplication
+function Multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+};
+//function to calculate division
+function Divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber
+};
+
+//function to clear calculator memory
+function clear() {
+   firstNumber = "";
+   operator = "";
+   secondNumber = ""; 
+}
